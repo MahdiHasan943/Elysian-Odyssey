e0755fb2902c01024a6ac0547db3d7e4bcff712b
unknown <mahdifreelancer41@gmail.com>
2024-03-24 17:52:44 +0600

    added new text

--- app/page.js
+++ app/page.js
@@ -2,18 +2,25 @@ import About from "@/components/About";
 import Charecters from "@/components/Charecters";
 import Gameplay from "@/components/Gameplay";
 import Hero from "@/components/Hero";
+import MultiLayerParallax from "@/components/MultiLayerParallax";
 import Roadmap from "@/components/Roadmap";
 import Team from "@/components/Team";
 import Test from "@/components/Test";
 import Tokenomics from "@/components/Tokenomics";
+import Button from "@/components/reusable/Button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="w-full">
       {/* <Hero /> */}
-      <Test />
-
+      {/* <Test /> */}
+      <MultiLayerParallax />
+      <div className="h-[400px] bg-[#210002] flex justify-center items-center py-20">
+        <Button className="mt-[-140px] uppercase mx-auto text-center  text-[#fff] ">
+          whitepaper
+        </Button>
+      </div>
       <About />
       <Charecters />
       {/* <Gameplay /> */}
