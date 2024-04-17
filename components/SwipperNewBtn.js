668b6be1283281282b17a4d465eafe15032d0c08
unknown <mahdifreelancer41@gmail.com>
2024-04-17 14:35:27 +0600

    making timeline

--- components/SwipperNewBtn.js
+++ components/SwipperNewBtn.js
@@ -12,16 +12,16 @@ export const SwipperNewBtn = () => {
     <div className="swiper-nav-btns    h-[300px] ">
       <div className="flex  tablet:flex-col gap-4 relative justify-between tablet:justify-center">
         <span
-          className=" animated-button  group rounded-full flex   relative  overflow-hidden justify-center items-center w-[20px] h-[30px]     sm:w-[50px] sm:h-[50px]"
+          className=" animated-button  group rounded-full flex   relative  overflow-hidden justify-center items-center w-[10px] h-[15px]     sm:w-[50px] sm:h-[50px]"
           onClick={() => swiper.slidePrev()}
         >
-          <BsArrowLeft className="text-[#111] group-hover:text-[#fff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[18px]" />
+          <BsArrowLeft className="text-[#111] group-hover:text-[#fff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-[10px] sm:text-[16px]" />
         </span>
         <span
-          className="bg-[#000]  tablet:hidden rounded-full animated-button group flex relative justify-center items-center w-[20px] h-[30px]     sm:w-[50px] sm:h-[50px]"
+          className="bg-[#000]  tablet:hidden rounded-full animated-button group flex relative justify-center items-center w-[10px] h-[15px]     sm:w-[50px] sm:h-[50px]"
           onClick={() => swiper.slideNext()}
         >
-          <BsArrowRight className="text-[#111] group-hover:text-[#fff]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[16px]" />
+          <BsArrowRight className="text-[#111] group-hover:text-[#fff]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-[10px] sm:text-[16px]" />
         </span>
       </div>
     </div>
@@ -35,16 +35,16 @@ export const SwipperNewBtn1 = () => {
     <div className="swiper-nav-btns hidden tablet:block   tablet:ml-[-200px] h-[300px] ">
       <div className="flex flex-col gap-4 relative justify-end">
         {/* <span
-          className=" animated-button  group rounded-full flex   relative  overflow-hidden justify-center items-center w-[20px] h-[30px]     sm:w-[50px] sm:h-[50px]"
+          className=" animated-button  group rounded-full flex   relative  overflow-hidden justify-center items-center w-[10px] h-[15px]     sm:w-[50px] sm:h-[50px]"
           onClick={() => swiper.slidePrev()}
         >
-          <BsArrowLeft className="text-[#111] group-hover:text-[#fff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[16px]" />
+          <BsArrowLeft className="text-[#111] group-hover:text-[#fff] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-[10px] sm:text-[16px]" />
         </span> */}
         <span
-          className="bg-[#000] mx-auto rounded-full animated-button group flex relative justify-end items-end w-[20px] h-[30px]     sm:w-[50px] sm:h-[50px]"
+          className="bg-[#000] mx-auto rounded-full animated-button group flex relative justify-end items-end w-[10px] h-[15px]     sm:w-[50px] sm:h-[50px]"
           onClick={() => swiper.slideNext()}
         >
-          <BsArrowRight className="text-[#111] group-hover:text-[#fff]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[18px]" />
+          <BsArrowRight className="text-[#111] group-hover:text-[#fff]  absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  text-[10px] sm:text-[16px]" />
         </span>
       </div>
     </div>
