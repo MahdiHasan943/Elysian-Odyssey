21eb6bb64245bc09f9b550599edeecbe0ae71945
unknown <mahdifreelancer41@gmail.com>
2024-07-07 17:44:20 +0600

    new updated

--- components/Charecters.js
+++ components/Charecters.js
@@ -13,8 +13,8 @@ const Charecters = () => {
   return (
     <div id="Characters">
       <div className="">
-        <div className=" min-h-[1100px]   lg:min-h-[1100px] lg:max-h-[1100px]  relative overflow-hidden ">
-          <div className=" absolute top-0 flex justify-center items-center w-full h-[400px] left-0 z-[50]">
+        <div className=" min-h-[900px]   lg:min-h-[1100px] lg:max-h-[1100px]  relative overflow-hidden ">
+          <div className=" absolute top-0 flex justify-center items-center w-full h-[200px] sm:h-[400px] left-0 z-[50]">
             <h2 className=" text-center sectionTitle text-[47px] md:text-[160px] larger:text-[200px]">
               CHARECTERS
             </h2>
@@ -29,30 +29,8 @@ const Charecters = () => {
               className=""
             />
             <div className="w-full px-4  py-10 absolute top-0 left-0 h-full z-[100000] max-auto">
-              {/* <motion.div
-                variants={staggerContainer}
-                initial="hidden"
-                whileInView="show"
-                viewport={{ once: false, amount: 0.25 }}
-                className=""
-              >
-                <TypingText
-                  title="Characters"
-                  textStyles={` text-center mt-20 text-[#f2d387] pt-5 pb-5 font-bold text-[40px] leading-[40px] uppercase `}
-                />
-              </motion.div> */}
-              {/* <Image
-                src={"/charecterTitle.png"}
-                alt="title"
-                width={800}
-                height={200}
-                className="mx-auto"
-              /> */}
-              {/* <h2 className="absolute top-0 left-0 z-[] sectionTitle text-[200px]">
-                CHARECTERS
-              </h2> */}
               <section
-                className={`sm:p-16 w-full max-w-[1600px]  mx-auto xs:p-8 px-6 py-12`}
+                className={`sm:p-16 w-full max-w-[1600px]  mt-[-70px] sm:mt-0 mx-auto xs:p-8 px-6 py-12`}
                 id="explore"
               >
                 <motion.div
